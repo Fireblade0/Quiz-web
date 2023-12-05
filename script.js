fed9eb6362496152ce7aa2c7d9c0bea07c03b2ac
Fireblade0 <151810855+Fireblade0@users.noreply.github.com>
2023-12-05 11:32:14 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,42 @@
+
+    gsap.from("#nav-bar1 h1:nth-child(1)",{
+        x:-300,
+    duration:1,
+    delay:0.2,
+    stagger:0.25,
+    opacity:0.3
+    })
+    
+        gsap.from("#nav-bar1 h1:nth-child(5)",{
+        x:300,
+        duration:1,
+        delay:0.2,
+        opacity:0.3,
+        stagger:0.25
+    })
+
+    gsap.from("#img-div h4:nth-child(1)",{
+        x:300,
+        duration:1.5,
+        delay:0.4,
+        stagger:0.30,
+        opacity:0.2
+
+    })
+
+    gsap.from("#img-div h4:nth-child(3)",{
+        y:200,
+        duration:1.5,
+        delay:0.4,
+        stagger:0.30,
+        opacity:0
+
+    })
+    gsap.from("#img-div h4:nth-child(5)",{
+        x:300,
+        duration:1,
+        delay:0.4,
+        stagger:0.30,
+        opacity:0.2
+
+    })
